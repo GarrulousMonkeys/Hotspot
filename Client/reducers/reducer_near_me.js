3f9b68837db4c0ca4ba128f1b788aa014a5a90d4
juliusbuckley <juliusbuckley@gmail.com>
2016-07-29 20:39:31 -0700

    /feat added reducer for near_me

---
+++ Client/reducers/reducer_near_me.js
@@ -0,0 +1,17 @@
+import { FETCH_NEAR_ME } from '../actions/index';
+
+const initialState = {
+  collection: []
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_NEAR_ME:
+      return {
+        ...state,
+        collection: action.payload.data.data
+      }
+    default:
+      return state;
+  }
+}
